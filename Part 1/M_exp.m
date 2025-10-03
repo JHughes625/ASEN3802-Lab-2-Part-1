5f36eee947d96332ab44b81adc7211911f54b557
grant65404 <grnatb05@gmail.com>
2025-10-03 15:12:56 -0600

    fixed M_exp so icould actually figure it out

--- Part 1/M_exp.m
+++ Part 1/M_exp.m
@@ -1,11 +1,9 @@
-function [g, Mexp] = M_exp(expData)
+function [g, Mexp, l] = M_exp(expData)
 l = linspace(0,8*0.0127);
-for i=1:5
-    initialVec(i,:) = expData.values(1,2:9);
-    T0(i) = initialVec(i,1);
+    initialVec = expData(1,2:9);
+    T0 = initialVec(1);
     x = 1:length(initialVec);
-    p = polyfit(x,initialVec(i,:),1);
-    Mexp(i) = p(1);
-    g(i) = Mexp(i)*l + T0(i);
-end
+    p = polyfit(x,initialVec,1);
+    Mexp = p(1);
+    g = Mexp*l + T0;
 end
