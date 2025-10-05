fc061d85016230ae354611f1d4f812189b5ce73b
JHughes625 <97568349+JHughes625@users.noreply.github.com>
2025-10-04 20:17:37 -0600

    Task 1 Code ~Complete see desc.
    Code produces a table of H_exp, H_an, & T_0 data & a plot of the LBF for the exp vs. analytical TC SSTD for each dataset.

--- Part 1/ASEN3802_Lab2_P1_Driver.m
+++ Part 1/ASEN3802_Lab2_P1_Driver.m
@@ -1,8 +1,15 @@
 clear all;
 clc;
 close all;
+% Heat Conduction Lab
+% Contributor(s): 
+% Jackson Hughes
+% Grant Bechtel
+% Course: ASEN3802
+% Date: 10/5/2025
 
-data_Files=dir('*mA');
+
+data_Files = dir('*mA');
 
 for i=1:length(data_Files)
     b = strsplit(data_Files(i).name,'_'); % gives a cell array (b) that is 1x3
@@ -20,11 +27,115 @@ for i=1:length(data_Files)
     expData(i).values = content_Files;
     clear content_Files;
 end
-%clear b ampval v i data_Files;
+clear b ampval v i data_Files volts amps content_Files;
+%% Material Properties
+in_to_m = 0.0254;
+
+rho_Aluminum = 2810; % kg/m^3
+rho_Brass = 8500; 
+rho_Steel = 8000; 
+
+cp_Aluminum = 960; % J/(kg*K)
+cp_Brass = 380; 
+cp_Steel = 500; 
+
+k_Aluminum = 130; % W/(m*K)
+k_Brass = 115;
+k_Steel = 16.2;
+
+%% Experimental Data
+
+x_0 = (1 + (3/8)) * in_to_m; % m
+radius_Rod = 0.5 * in_to_m; % m
+area_Rod = radius_Rod^2 * pi; % m^2
+L_Rod = x_0 + (0.5 * in_to_m * 7) + (1 * in_to_m); % m 
+TC_Positions = [
+    x_0 ...
+    (x_0 + 1*(0.5 * in_to_m)) ...
+    (x_0 + 2*(0.5 * in_to_m)) ...
+    (x_0 + 3*(0.5 * in_to_m)) ...
+    (x_0 + 4*(0.5 * in_to_m)) ...
+    (x_0 + 5*(0.5 * in_to_m)) ...
+    (x_0 + 6*(0.5 * in_to_m)) ...
+    (x_0 + 7*(0.5 * in_to_m)) ...
+    ];
 
 for i=1:length(expData)
-    [H_exp(i), H_an(i), LBF_exp(i), LBF_an(i), T_0(i)] = P1_SSTD(expData(i));
+    [H_exp(i), LBF_exp(:,i), H_an(i), LBF_an(:,i), T_0(i)] = P1_SSTD(expData(i));
 end
+
+
+
+%% Plots & Tables
+
+outputTable = table(T_0,H_an,H_exp)
+
+figure
+hold on;
+plot(expData(1).values(:,1), expData(1).values(:,2));
+plot(expData(1).values(:,1), expData(1).values(:,3));
+plot(expData(1).values(:,1), expData(1).values(:,4));
+plot(expData(1).values(:,1), expData(1).values(:,5));
+plot(expData(1).values(:,1), expData(1).values(:,6));
+plot(expData(1).values(:,1), expData(1).values(:,7));
+plot(expData(1).values(:,1), expData(1).values(:,8));
+plot(expData(1).values(:,1), expData(1).values(:,9));
+xlabel('Time [s]');
+ylabel('Temperature [C]')
+title('Aluminum 25V 240mA Thermocouple');
+legend('Thermocouple 1', 'Thermocouple 2', 'Thermocouple 3', 'Thermocouple 4', ...
+    'Thermocouple 5', 'Thermocouple 6', 'Thermocouple 7', 'Thermocouple 8');
+hold off;
+
+figure
+hold on
+plot(TC_Positions,LBF_an(:,1));
+plot(TC_Positions,LBF_exp(:,1));
+xlabel('Position [m]');
+ylabel('Temperature [C]')
+title('Aluminum 25V 240mA SSTD Comparison');
+legend('[An]','[Exp]');
+hold off
+
+figure
+hold on
+plot(TC_Positions,LBF_an(:,2));
+plot(TC_Positions,LBF_exp(:,2));
+xlabel('Position [m]');
+ylabel('Temperature [C]')
+title('Aluminum  30V 290mA SSTD Comparison');
+legend('[An]','[Exp]');
+hold off
+
+figure
+hold on
+plot(TC_Positions,LBF_an(:,3));
+plot(TC_Positions,LBF_exp(:,3));
+xlabel('Position [m]');
+ylabel('Temperature [C]')
+title('Brass  25V 237mA SSTD Comparison');
+legend('[An]','[Exp]');
+hold off
+
+figure
+hold on
+plot(TC_Positions,LBF_an(:,4));
+plot(TC_Positions,LBF_exp(:,4));
+xlabel('Position [m]');
+ylabel('Temperature [C]')
+title('Brass  30V 285mA SSTD Comparison');
+legend('[An]','[Exp]');
+hold off
+
+figure
+hold on
+plot(TC_Positions,LBF_an(:,5));
+plot(TC_Positions,LBF_exp(:,5));
+xlabel('Position [m]');
+ylabel('Temperature [C]')
+title('Steel 22V 203mA SSTD Comparison');
+legend('[An]','[Exp]');
+hold off
 
 for j=1:length(expData) %TASK 2 Plotting
 [g(j,:),M(j),x_L] = M_exp(expData(j).values);
--- Part 1/P1_SSTD.m
+++ Part 1/P1_SSTD.m
@@ -1,28 +1,36 @@
-function [H_exp, H_an, LBF_exp, LBF_an, T_0] = P1_SSTD(expData)
+% Heat Conduction Lab
+% Contributor(s): 
+% Jackson Hughes
+% Course: ASEN3802
+% Date: 10/5/2025
+
+function [H_exp, LBF_exp, H_an, LBF_an, T_0] = P1_SSTD(expData)
 %% Material Properties
 in_to_m = 0.0254;
 
-rho_Aluminum = 2810; % kg/m^3
+rho_Aluminum = 2810; % [kg/m^3]
 rho_Brass = 8500; 
 rho_Steel = 8000; 
 
-cp_Aluminum = 960; % J/(kg*K)
+cp_Aluminum = 960; % [J/(kg*K)]
 cp_Brass = 380; 
 cp_Steel = 500; 
 
-k_Aluminum = 130; % W/(m*K)
+k_Aluminum = 130; % [W/(m*K)]
 k_Brass = 115;
 k_Steel = 16.2;
 
 %% Experimental Data
 
-x_0 = (1 + (3/8)) * in_to_m; % m
-radius_Rod = 0.5 * in_to_m; % m
-area_Rod = radius_Rod^2 * pi; % m^2
-L_Rod = x_0 + (0.5 * in_to_m * 7) + (1 * in_to_m); % m 
+x_0 = (1 + (3/8)) * in_to_m; % [m]
+radius_Rod = 0.5 * in_to_m; % [m]
+area_Rod = radius_Rod^2 * pi; % [m^2]
+L_Rod = x_0 + (0.5 * in_to_m * 7) + (1 * in_to_m); % [m] 
 
 %% Define Material Properties
-    b = strsplit(expData.name,'_'); % gives a cell array (b) that is 1x3
+
+% Take in name string and detect which material to use
+    b = strsplit(expData.name,'_');
     if strcmp(b{1}, 'Aluminum') == 1
         rho_mat = rho_Aluminum;
         cp_mat = cp_Aluminum;
@@ -36,7 +44,8 @@ L_Rod = x_0 + (0.5 * in_to_m * 7) + (1 * in_to_m); % m
         cp_mat = cp_Steel;
         k_mat = k_Steel;
     end
-TC_Positions = [
+% Define locations of thermocouples
+TC_Data(1,:) = [
     x_0 ...
     (x_0 + 1*(0.5 * in_to_m)) ...
     (x_0 + 2*(0.5 * in_to_m)) ...
@@ -48,10 +57,26 @@ TC_Positions = [
     ];
 
 %% Analytical
-H_an = (expData.volts * expData.amps * (1/1000)) / (area_Rod * k_mat);
 
-    H_exp = 0;
-    LBF_exp = 0;
-    LBF_an = 0;
-    T_0 = 0;
+% assume T_0 is the same as the initial temp of the TC closest to cold end
+T_0 = expData.values(1,2); % [C]
+
+%use given formula to calc. H_an
+H_an = (expData.volts * expData.amps * (1/1000)) / (area_Rod * k_mat); % [C/m]
+LBF_an = H_an .* TC_Data(:) + T_0;
+
+%% Experimental
+
+for i = 2:length(expData(1).values(1,:))
+    % Take the mean of the values in the range of the steady state length
+    % of the data
+    TC_Data(2,i-1) = mean(expData.values(150:length(expData.values)-2,i));
+end
+% use polyfit to generate a Line of Best Fit for the dataset
+[polycoeff_1] = polyfit(TC_Data(1,:),TC_Data(2,:), 1);
+
+% pass soln. values to function
+H_exp = polycoeff_1(1); % [C/m]
+LBF_exp = polycoeff_1(1).*TC_Data(1,:) + polycoeff_1(2);
+
 end
